1ceb26a21761da69de0e28b1a25945ac15253741
ivana <ivana.kruk@gmail.com>
2021-08-20 11:32:12 -0300

    arreglo en menu desplegable

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 
+
+const menuLinks = document.querySelectorAll(".nav-menu a[href^=\"#\"]")
+
+menuLinks.forEach(menuLinks => {
+    menuLinks.addEventListener("click", function(){
+        navMenu.classList.remove("nav-menu_visible");
+    })
+})
+
 navToggle.addEventListener("click", ()=> {
     navMenu.classList.toggle("nav-menu_visible")
 
